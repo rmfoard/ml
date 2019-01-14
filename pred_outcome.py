9cd0415d57fc6cb8fadb3ac550a3cb97adf37ab5
Richard Foard <rfoard@jhu.edu>
2019-01-14 15:12:44 -0500

    Began composing outcome predictor 'pred_outc.py'

--- pred_outcome.py
+++ pred_outcome.py
@@ -27,4 +27,10 @@ model.add(Dense(1, activation='sigmoid'))
 # Compile the model.
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
+# Fit the model.
+model.fit(X, Y, epochs=150, batch_size=10)
+
+# Evaluate the model.
+scores = model.evaluate(X, Y)
+print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
 sys.exit(0)
