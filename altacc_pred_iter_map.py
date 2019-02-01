63c9e316d19cd78833f30a7a34c13884800c8ff5
Richard Foard <rfoard@jhu.edu>
2019-02-01 10:54:04 -0500

    Add altacc_pred_iter_map.py (K/L metric trials)

---
+++ altacc_pred_iter_map.py
@@ -0,0 +1,43 @@
+"""Drawn from Jason Brownlee's example dense network, from:
+https://machinelearningmastery.com/tutorial-first-neural-network-python-keras/
+"""
+import numpy
+import sys
+
+from keras.models import Sequential
+from keras.layers import Dense
+from keras import optimizers
+
+
+# Fix the random seed for reproducibility.
+numpy.random.seed(7)
+
+# Load the ruleparts + outcomes data.
+dataset = numpy.loadtxt("C1031yitersmap.csv", delimiter=",")
+
+# split into input (X) and output (Y) variables
+X = dataset[:,2:114]
+Y = dataset[:,1]
+
+# Create the model.
+model = Sequential()
+model.add(Dense(112, input_dim=112, activation='relu'))
+model.add(Dense(256, activation='relu'))
+#model.add(Dense(1, activation='relu'))
+model.add(Dense(1))
+
+# Set up an optimizer.
+sgd = optimizers.SGD(lr=0.1)
+
+# Compile the model.
+model.compile(loss='mean_absolute_error', optimizer=sgd, metrics=['accuracy', 'kullback_leibler_divergence'])
+print(model.metrics)
+
+# Fit the model.
+model.fit(X, Y, epochs=150, batch_size=10)
+
+# Evaluate the model.
+scores = model.evaluate(X, Y)
+print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
+print("\n%s: %.6f%%" % (model.metrics_names[2], scores[2]))
+sys.exit(0)
